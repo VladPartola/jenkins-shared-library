d9fe62c29bbd37c1df361474ba3195d10e6479ec
VladPartola <74604856+VladPartola@users.noreply.github.com>
2021-10-14 13:48:56 +0300

    Update
    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,7 +1,9 @@
 #!/usr/bin/env groovy
 
-import com.example.Docker
-
-def call(String imageName) {
-    return new Docker(this).buildDockerImage(imageName)
-}
+def call() {
+echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-key', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+        sh 'docker build -t vladpartola/java-maven-app:jma-2.0 .'
+        sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
+        sh 'docker push vladpartola/java-maven-app:jma-2.0'
+    }
