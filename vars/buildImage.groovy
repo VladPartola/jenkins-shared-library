3a736a18efc1bd00a22a55970878e5f6ab80e5d7
VladPartola <74604856+VladPartola@users.noreply.github.com>
2021-10-14 14:13:48 +0300

    Update image version

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -3,8 +3,8 @@
 def call() {
 echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-key', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-        sh 'docker build -t vladpartola/java-maven-app:jma-2.0 .'
+        sh 'docker build -t vladpartola/java-maven-app:jma-3.0 .'
         sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
-        sh 'docker push vladpartola/java-maven-app:jma-2.0'
+        sh 'docker push vladpartola/java-maven-app:jma-3.0'
     }
 }
